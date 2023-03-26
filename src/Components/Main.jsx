97e8ebda66ad4cc6b47fc0ae12b802db49c500e0
Priya Tomar <priya.paradoxalpha@gmail.com>
2023-03-26 19:49:49 +0530

    Hart position change

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -8,72 +8,68 @@ import CardSection from './CardSection';
 const Image=styled.img`
   height: 64px;
   width:64px;
-  margin-top: 30px; 
-  margin-left: 40px;
+  margin-top: 40px; 
+  margin-left: 50px;
   @media screen and (max-width: 500px){
     height: 54px;
   width:54px;
-  margin-top: 15px; 
+  margin-top: 25px; 
   margin-left: 20px;
 };
 `
 const Heading1=styled.h1`
   color:white;
   font-size: 45px;
-  margin-left: 40px;
-  margin-top:-10px;
+  margin-left: 60px;
+  margin-top:40px;
   font-weight:600;
  font-family: Inter,Helvetica,Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji,Noto Color Emoji,Segoe UI Symbol,Android Emoji,EmojiSymbols,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Noto Sans,sans-serif;
  @media screen and (max-width: 500px){
     font-size: 35px;
-    margin-left: 20px;
-  margin-top:-30px;
+  margin-left: 20px;
+  margin-top:25px;
 };
 `
+const Hartlogo=styled.div`
+  display:flex;
+  flex-direction:row;
+`
 const Container1=styled.div`
-  padding: 30px 0px;
   display:flex;
   flex-direction:column;
   flex-wrap: nowrap;
   @media screen and (max-width: 500px){
-    padding: 15px 0px;
+    flex-direction:column;
 };
 `
 const Container2=styled.div`
-  padding: 30px 0px;
   display:flex;
   flex-direction:row;
   flex-wrap: nowrap;
   @media screen and (max-width: 500px){
     flex-direction:column;
-    /* border:2px solid red; */
 };
 `
 const Heading2=styled.div`
    color:white;
   font-size: 51px;
   margin-top:100px;
-  font-weight:600;
-  height:90px;
-  width:550px;
-  /* border:2px solid yellow; */
-  margin-left:-110px;
+  margin-left:30px;
  font-family: Inter,Helvetica,Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji,Noto Color Emoji,Segoe UI Symbol,Android Emoji,EmojiSymbols,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Noto Sans,sans-serif; 
   @media screen and (max-width: 500px){
-    height:140px;
-  width:250px;
   font-size: 36px;
-  font-weight:600;
-    /* border:2px solid yellow; */
-    margin-left:-85px;
+  text-align:left;
+    margin-left:15px;
     margin-top:40px;
 };
 `
 const ContainerLeft=styled.div`
 margin-top:30px;
   display:flex;
+  flex-direction:column;
   width:660px; 
   height:545px;
+  margin-left:30px;
   @media screen and (max-width: 500px){
     width:360px; 
   height:245px;
@@ -87,36 +83,32 @@ const ContainerRight=styled.div`
   height:245px;
   margin-top:150px;
   margin-left:30px;
-  /* border:2px solid green; */
 };
 `
 const Para=styled.p`
   font-size:19px;
   color:white;
-  height: 40px;
-  width: 103%;
-  margin-top:260px;
-  margin-left:-550px;
-  /* border:2px solid yellow; */
+  margin-top:50px;
+  font-weight:0;
+  margin-left:30px;
   @media screen and (max-width: 500px){
-  margin-top:300px;
-  margin-left:-240px;
+  margin-top:30px;
+  margin-left:20px;
   font-size:17px;
-  height: 30px;
-  width: 85%;
 };
 `
-const Button1=styled.form`
+const Button1=styled.a`
   height:45px; 
   width:255px; 
  background-color:#23CFA4;
-  margin-top:390px;
-  margin-left:-670px;
+  margin-top:20px;
+  margin-left:30px;
   border-radius:25px;
-  padding:2px -12px;
-  border:none;
+  padding:10px 1px;
+  text-align: center;
+  text-decoration: none;
   color:black;
-  font-weight:550;
+  font-weight:400;
   @media screen and (max-width: 500px){
     font-size: 25px;
     height:30px; 
@@ -137,42 +129,28 @@ margin-left:87px;
   margin-left:22px;
 };
 `
-const Input1=styled.input`
-   color:black;
-  font-weight:550;
-  background-color:#23CFA4;
-  font-size:17px;
-  margin-left:25px;
-  border:none; 
-  margin-top:10px;
-  @media screen and (max-width: 500px){
-    margin-left:15px;
-    font-size:15px;
-    margin-top:3px;
-  }
-`
+
+
 const Main = () => {
   return (
     <Container1>
-    
+      <Hartlogo>      
       <Image src={Logo} alt='image1'></Image>
+      <Heading1>Hart<span style={{color:"#23CFA4"}}>.</span></Heading1>
+      </Hartlogo>
       <Container2>
       <ContainerLeft>
-      <Heading1>Hart<span style={{color:"#23CFA4"}}>.</span></Heading1>
       <Heading2>Store All Your <span style={{color:"#23CFA4"}}>Medical    Documents</span> Online<span style={{color:"#23CFA4"}}>.</span></Heading2>
       <Para>Also track your <span style={{color:"#23CFA4"}}>family's medical history </span>and share your prescriptions and test reports with doctors <span style={{color:"#23CFA4"}}>securely in one-click.</span></Para>
-      {/* <Button1 action=''style={{fontSize:'17px'}}>Register for early access</Button1> */}
-      <Button1 action="https://docs.google.com/forms/d/e/1FAIpQLSfMp7EwAKtKYbvzCsNgXdwqGk-PHm1hPhpuZuvwWj-s81rQqg/viewform" style={{fontSize:'17px'}}>   
-        <Input1 type="submit" value="Register for early access"/>
-        </Button1>
-      <AiOutlineArrowRight className='arrow' style={{color:"#080808"}}></AiOutlineArrowRight>
-      </ContainerLeft>
+      <Button1 target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSfMp7EwAKtKYbvzCsNgXdwqGk-PHm1hPhpuZuvwWj-s81rQqg/viewform" style={{fontSize:'17px'}}>Register for early access </Button1>
+      <AiOutlineArrowRight className='arrow' style={{color:"#151514"}}></AiOutlineArrowRight>
+      </ContainerLeft> 
       <ContainerRight>
         <Image1 src={Phone} alt='Image2'></Image1>
-      </ContainerRight>
+       </ContainerRight>
       </Container2>
       <CardSection></CardSection>
-    </Container1>
+      </Container1>
     
   )
 }
